849ad36b43a35e5b5b05b80b1f9602a78ed16c1f
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-04-25 18:35:52 +0500

    first commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,71 @@
+import { Box, Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { AiFillHome } from "react-icons/ai";
+import { RxAvatar } from "react-icons/rx";
+import { Link as RouterLink } from "react-router-dom";
+import { FiLogOut } from "react-icons/fi";
+import useLogout from "../hooks/useLogout";
+import authScreenAtom from "../atoms/authAtom";
+import { BsFillChatQuoteFill } from "react-icons/bs";
+import { MdOutlineSettings } from "react-icons/md";
+
+const Header = () => {
+	const { colorMode, toggleColorMode } = useColorMode();
+	const user = useRecoilValue(userAtom);
+	const logout = useLogout();
+	const setAuthScreen = useSetRecoilState(authScreenAtom);
+
+	return (
+		<Flex alignItems={"center"} mt={6} mb='12' gap={20}>
+			<Box bg={"white"} borderRadius={6} p={2}>
+				<Image
+					cursor={"pointer"}
+					alt='logo'
+					w={20}
+					src={colorMode === "light" ? "/chefsphere_logo.svg" : "/chefsphere_logo.svg"}
+				// onClick={toggleColorMode}
+				/>
+			</Box>
+
+			{user && (
+				<Link as={RouterLink} to='/'>
+					{/* <AiFillHome size={24} /> */}
+					Home
+				</Link>
+			)}
+			{!user && (
+				<Link as={RouterLink} to={"/auth"} onClick={() => setAuthScreen("login")}>
+					Login
+				</Link>
+			)}
+
+			{user && (
+				<Flex alignItems={"center"} gap={16}>
+					<Link as={RouterLink} to={`/${user.username}`}>
+						{/* <RxAvatar size={24} /> */}
+						Profile
+					</Link>
+					<Link as={RouterLink} to={`/chat`}>
+						{/* <BsFillChatQuoteFill size={20} /> */}
+						Chat
+					</Link>
+					{/* <Link as={RouterLink} to={`/settings`}>
+						<MdOutlineSettings size={20} />
+					</Link> */}
+					<Button size={"xs"} onClick={logout}>
+						<FiLogOut size={20} />
+					</Button>
+				</Flex>
+			)}
+
+			{!user && (
+				<Link as={RouterLink} to={"/auth"} onClick={() => setAuthScreen("signup")}>
+					Sign up
+				</Link>
+			)}
+		</Flex>
+	);
+};
+
+export default Header;
---
+++ frontend/src/components/UserHeader.jsx
@@ -0,0 +1,73 @@
+import { Avatar } from "@chakra-ui/avatar";
+import { Box, Flex, Link, Text, VStack } from "@chakra-ui/layout";
+import { Button, useToast } from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { Link as RouterLink } from "react-router-dom";
+import useFollowUnfollow from "../hooks/useFollowUnfollow";
+
+const UserHeader = ({ user }) => {
+	const toast = useToast();
+	const currentUser = useRecoilValue(userAtom); // logged in user
+	const { handleFollowUnfollow, following, updating } = useFollowUnfollow(user);
+
+
+
+	return (
+		<VStack gap={4} alignItems={"start"}>
+			<Flex justifyContent={"space-between"} w={"full"}>
+				<Box>
+					<Text fontSize={"2xl"} fontWeight={"bold"}>
+						{user.name}
+					</Text>
+					<Flex gap={2} alignItems={"center"}>
+						<Text fontSize={"sm"}>{user.username}</Text>
+					</Flex>
+				</Box>
+				<Box>
+					{user.profilePic && (
+						<Avatar
+							name={user.name}
+							src={user.profilePic}
+							size={{
+								base: "md",
+								md: "xl",
+							}}
+						/>
+					)}
+					{!user.profilePic && (
+						<Avatar
+							name={user.name}
+							src='https://bit.ly/broken-link'
+							size={{
+								base: "md",
+								md: "xl",
+							}}
+						/>
+					)}
+				</Box>
+			</Flex>
+
+			<Text fontStyle={"italic"}>{user.bio}</Text>
+
+			{currentUser?._id === user._id && (
+				<Link as={RouterLink} to='/update'>
+					<Button size={"sm"}>Update Profile</Button>
+				</Link>
+			)}
+			{currentUser?._id !== user._id && (
+				<Button size={"sm"} onClick={handleFollowUnfollow} isLoading={updating}>
+					{following ? "Unfollow" : "Follow"}
+				</Button>
+			)}
+
+			<Flex w={"full"}>
+				<Flex flex={1} borderBottom={"1.5px solid white"} justifyContent={"center"} pb='3' cursor={"pointer"}>
+					<Text fontWeight={"bold"}>Posts</Text>
+				</Flex>
+			</Flex>
+		</VStack>
+	);
+};
+
+export default UserHeader;
